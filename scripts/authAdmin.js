17a5bf2f34d765062fbb98b5c4005a4860c7b3b0
godswillokokon <godswillokokon3@gmail.com>
2020-02-19 23:56:26 +0100

    rm authAdmin

--- scripts/authAdmin.js
+++ scripts/authAdmin.js
@@ -15,24 +15,24 @@
 //     console.log("admin not logged in");
 //   }
 // });
-let currentUser = sessionStorage.getItem("admin");
+// let currentUser = sessionStorage.getItem("admin");
 
-if (currentUser) {
-  console.log("admin logged in from auth");
+// if (currentUser) {
+//   console.log("admin logged in from auth");
 
-  // user.getIdTokenResult().then(idTokenResult => {
-  //   user.admin = idTokenResult.claims.admin;
-  //   setupUI(user);
-  // });
-  // db.collection("guides").onSnapshot(
-  //   snapshot => {
-  //     setupGuides(snapshot.docs);
-  //   },
-} else {
-  location.href = "index.html";
-  err => console.log(err.message);
-  console.log("admin not logged in from auth");
-}
+//   // user.getIdTokenResult().then(idTokenResult => {
+//   //   user.admin = idTokenResult.claims.admin;
+//   //   setupUI(user);
+//   // });
+//   // db.collection("guides").onSnapshot(
+//   //   snapshot => {
+//   //     setupGuides(snapshot.docs);
+//   //   },
+// } else {
+//   location.href = "index.html";
+//   err => console.log(err.message);
+//   console.log("admin not logged in from auth");
+// }
 function logout() {
   //  e.preventDefault();
   // clearCookie();
